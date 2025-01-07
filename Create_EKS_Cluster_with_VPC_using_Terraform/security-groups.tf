720d4b85bbc88f850b7fc2666bfbefc5a8b9b1c9
men_l <men_long@yahoo.com>
2025-01-07 16:35:07 -0500

    update

--- Create_EKS_Cluster_with_VPC_using_Terraform/security-groups.tf
+++ Create_EKS_Cluster_with_VPC_using_Terraform/security-groups.tf
@@ -1,29 +1,44 @@
-
-resource "aws_security_group" "all_worker_mgmt" {
-  name_prefix = "all_worker_management"
-  vpc_id      = module.vpc.vpc_id
+# Outputs the EKS (Elastic Kubernetes Service) cluster ID.
+output "cluster_id" {
+  # Description of the output variable.
+  description = "EKS cluster ID."
+  # The value for the output, which retrieves the cluster_id from the EKS module.
+  value       = module.eks.cluster_id
 }
 
-resource "aws_security_group_rule" "all_worker_mgmt_ingress" {
-  description       = "allow inbound traffic from eks"
-  from_port         = 0
-  protocol          = "-1"
-  to_port           = 0
-  security_group_id = aws_security_group.all_worker_mgmt.id
-  type              = "ingress"
-  cidr_blocks = [
-    "10.0.0.0/8",
-    "172.16.0.0/12",
-    "192.168.0.0/16",
-  ]
+# Outputs the endpoint for the EKS control plane.
+output "cluster_endpoint" {
+  # Description of the output variable.
+  description = "Endpoint for EKS control plane."
+  # The value for the output, which retrieves the cluster_endpoint from the EKS module.
+  value       = module.eks.cluster_endpoint
 }
 
-resource "aws_security_group_rule" "all_worker_mgmt_egress" {
-  description       = "allow outbound traffic to anywhere"
-  from_port         = 0
-  protocol          = "-1"
-  security_group_id = aws_security_group.all_worker_mgmt.id
-  to_port           = 0
-  type              = "egress"
-  cidr_blocks       = ["0.0.0.0/0"]
+# Outputs the security group IDs attached to the EKS cluster control plane.
+output "cluster_security_group_id" {
+  # Description of the output variable.
+  description = "Security group ids attached to the cluster control plane."
+  # The value for the output, which retrieves the security group IDs from the EKS module.
+  value       = module.eks.cluster_security_group_id
 }
+
+# Outputs the AWS region in which the EKS cluster is deployed.
+output "region" {
+  # Description of the output variable.
+  description = "AWS region"
+  # The value for the output, which is set to the AWS region variable.
+  value       = var.aws_region
+}
+
+# Outputs the ARN (Amazon Resource Name) of the OIDC (OpenID Connect) provider associated with the EKS cluster.
+output "oidc_provider_arn" {
+  # The value for the output, which retrieves the OIDC provider ARN from the EKS module.
+  value = module.eks.oidc_provider_arn
+}
+
+# Uncomment the following lines if you want to output the command to update the kubeconfig file for the EKS cluster.
+# output "zz_update_kubeconfig_command" {
+#   # The value for the output, which formats a command to update the kubeconfig file for the EKS cluster.
+#   # The format function is used to concatenate the command string and variables.
+#   value = format("%s %s %s %s", "aws eks update-kubeconfig --name", module.eks.cluster_id, "--region", var.aws_region)
+# }
