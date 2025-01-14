7603e00a70b0eb76cf29f9e6000ed3019c4632f8
men_l <men_long@yahoo.com>
2025-01-14 18:26:24 -0500

    update

--- Terraform-Jenkins-AWSEks/Jenkins-Server/jenkins-install.sh
+++ Terraform-Jenkins-AWSEks/Jenkins-Server/jenkins-install.sh
@@ -6,7 +6,8 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade -y
-sudo amazon-linux-extras install java-openjdk17 -y
+# sudo amazon-linux-extras install java-openjdk17 -y
+sudo yum install java-17-amazon-corretto-devel -y
 sudo yum install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
