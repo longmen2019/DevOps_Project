86e1bc76152f1fc2a4d56441972d8c1a0309cb0a
men_l <men_long@yahoo.com>
2025-01-16 17:44:42 -0500

    update

--- Terraform-Jenkins-AWSEks/EKS/provider.tf
+++ Terraform-Jenkins-AWSEks/EKS/provider.tf
@@ -1,11 +1,4 @@
-provider "aws" {
-  region = "us-east-1"
-}
+# provider "aws" {
+#   region = "us-east-1"
+# }
 
-resource "aws_cloudwatch_log_group" "this" {
-  name = "/aws/eks/my-eks-cluster/cluster"
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
