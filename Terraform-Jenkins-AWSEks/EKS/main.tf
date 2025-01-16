b9eba5612f4f7f6e9f0d4254136c7073cfe411e9
men_l <men_long@yahoo.com>
2025-01-16 17:44:42 -0500

    update

--- Terraform-Jenkins-AWSEks/EKS/main.tf
+++ Terraform-Jenkins-AWSEks/EKS/main.tf
@@ -1,8 +1,19 @@
-data "aws_cloudwatch_log_group" "existing" { 
-  name = "/aws/eks/my-eks-cluster/cluster" } 
-  resource "aws_cloudwatch_log_group" "this" {
-     count = data.aws_cloudwatch_log_group.existing.id == "" ? 1 : 0 
-     name = "/aws/eks/my-eks-cluster/cluster" lifecycle { prevent_destroy = true } }
+provider "aws" {
+  region = var.region
+}
+
+data "aws_cloudwatch_log_group" "existing" {
+  name = "/aws/eks/my-eks-cluster/cluster"
+}
+
+resource "aws_cloudwatch_log_group" "this" {
+  count = data.aws_cloudwatch_log_group.existing.id == "" ? 1 : 0
+  name  = "/aws/eks/my-eks-cluster/cluster"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
@@ -60,61 +71,3 @@ module "eks" {
     Terraform   = "true"
   }
 }
-
-# module "vpc" {
-#   source = "terraform-aws-modules/vpc/aws"
-
-#   name = "jenkins-vpc"
-#   cidr = var.vpc_cidr
-
-#   azs = data.aws_availability_zones.azs.names
-
-#   private_subnets = var.private_subnets
-#   public_subnets  = var.public_subnets
-
-#   enable_dns_hostnames = true
-#   enable_nat_gateway   = true
-#   single_nat_gateway   = true
-
-#   tags = {
-#     "kubernetes.io/cluster/my-eks-cluster" = "shared"
-#   }
-
-#   public_subnet_tags = {
-#     "kubernetes.io/cluster/my-eks-cluster" = "shared"
-#     "kubernetes.io/role/elb"               = 1
-#   }
-
-#   private_subnet_tags = {
-#     "kubernetes.io/cluster/my-eks-cluster" = "shared"
-#     "kubernetes.io/role/internal-elb"      = 1
-#   }
-
-# }
-
-# module "eks" {
-#   source = "terraform-aws-modules/eks/aws"
-
-#   cluster_name    = "my-eks-cluster"
-#   cluster_version = "1.24"
-
-#   cluster_endpoint_public_access = true
-
-#   vpc_id     = module.vpc.vpc_id
-#   subnet_ids = module.vpc.private_subnets
-
-#   eks_managed_node_groups = {
-#     nodes = {
-#       min_size     = 1
-#       max_size     = 3
-#       desired_size = 2
-
-#       instance_type = ["t2.small"]
-#     }
-#   }
-
-#   tags = {
-#     Environment = "dev"
-#     Terraform   = "true"
-#   }
-# }
