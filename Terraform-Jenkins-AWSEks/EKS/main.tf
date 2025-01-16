9bad2a5995fcdf2f16f22e94ccaa9cc69c5db358
men_l <men_long@yahoo.com>
2025-01-16 16:31:06 -0500

    update

--- Terraform-Jenkins-AWSEks/EKS/main.tf
+++ Terraform-Jenkins-AWSEks/EKS/main.tf
@@ -1,56 +1,57 @@
-#vpc.tf
-data "aws_availability_zones" "azs" {}
-module "myjenkins-server-vpc" {
-  source          = "terraform-aws-modules/vpc/aws"
-  name            = "myjenkins-server-vpc"
-  cidr            = var.vpc_cidr_block
-  private_subnets = var.private_subnet_cidr_blocks
-  public_subnets  = var.public_subnet_cidr_blocks
-  azs             = data.aws_availability_zones.azs.names
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
+  name = "jenkins-vpc"
+  cidr = var.vpc_cidr
+
+  azs = data.aws_availability_zones.azs.names
+
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+
+  enable_dns_hostnames = true
   enable_nat_gateway   = true
   single_nat_gateway   = true
-  enable_dns_hostnames = true
 
   tags = {
-    "kubernetes.io/cluster/myjenkins-server-eks-cluster" = "shared"
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
   }
 
   public_subnet_tags = {
-    "kubernetes.io/cluster/myjenkins-server-eks-cluster" = "shared"
-    "kubernetes.io/role/elb"                  = 1
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/role/elb"               = 1
   }
 
   private_subnet_tags = {
-    "kubernetes.io/cluster/myjenkins-server-eks-cluster" = "shared"
-    "kubernetes.io/role/internal-elb"         = 1
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/role/internal-elb"      = 1
   }
+
 }
 
-#eks-cluster.tf
 module "eks" {
-    source  = "terraform-aws-modules/eks/aws"
-    version = "~> 19.0"
-    cluster_name = "myjenkins-server-eks-cluster"
-    cluster_version = "1.24"
+  source = "terraform-aws-modules/eks/aws"
 
-    cluster_endpoint_public_access  = true
+  cluster_name    = "my-eks-cluster"
+  cluster_version = "1.24"
 
-    vpc_id = module.myjenkins-server-vpc.vpc_id
-    subnet_ids = module.myjenkins-server-vpc.private_subnets
+  cluster_endpoint_public_access = true
 
-    tags = {
-        environment = "development"
-        application = "myjenkins-server"
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    nodes = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+
+      instance_type = ["t2.small"]
     }
+  }
 
-    eks_managed_node_groups = {
-        dev = {
-            min_size = 1
-            max_size = 3
-            desired_size = 2
-
-            instance_types = ["t2.small"]
-        }
-    }
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
 }
