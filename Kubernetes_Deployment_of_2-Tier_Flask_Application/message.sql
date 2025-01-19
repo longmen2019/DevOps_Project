850706376d4df8140b2db31235472eb9f5623890
men_l <men_long@yahoo.com>
2025-01-19 12:25:42 -0500

    update

--- Kubernetes_Deployment_of_2-Tier_Flask_Application/message.sql
+++ Kubernetes_Deployment_of_2-Tier_Flask_Application/message.sql
@@ -1,4 +1,4 @@
-CREATE TABLE messages (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    message TEXT
+CREATE TABLE messages (  -- Create a new table named 'messages'
+    id INT AUTO_INCREMENT PRIMARY KEY,  -- Define a column 'id' as an integer with auto-incrementing values, and set it as the primary key for the table
+    message TEXT  -- Define a column 'message' to store text data
 );
