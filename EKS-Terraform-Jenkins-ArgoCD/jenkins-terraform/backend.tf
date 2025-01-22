bb1274c417f35624b214aeb34b9fdc428893d140
men_l <men_long@yahoo.com>
2025-01-22 16:54:55 -0500

    update

--- EKS-Terraform-Jenkins-ArgoCD/jenkins-terraform/backend.tf
+++ EKS-Terraform-Jenkins-ArgoCD/jenkins-terraform/backend.tf
@@ -1,16 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "devops-tetris-project"
-    region         = "us-east-1"
-    key            = "devops-demo/jenkins/terraform.tfstate"
-    dynamodb_table = "dynamodb-state-locking"
-    encrypt        = true
-  }
-  required_version = ">=0.13.0"
-  required_providers {
-    aws = {
-      version = ">= 2.7.0"
-      source  = "hashicorp/aws"
-    }
+    bucket = "cicd-terraform-eks-longmen"
+    key    = "jenkins/terraform.tfstate"
+    region = "us-east-1"
   }
 }
