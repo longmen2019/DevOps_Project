6b3593ece218e126d9b9496a4016e5742f7970b2
men_l <men_long@yahoo.com>
2025-01-27 08:34:29 -0500

    update

--- Three_Tier_DevSecOps_Kubernetes_Project_on_AWS_EKS_with_ArgoCD_Prometheus_Grafana_Jenkins/jenkins-server-terraform/07_iam-instance-profile.tf
+++ Three_Tier_DevSecOps_Kubernetes_Project_on_AWS_EKS_with_ArgoCD_Prometheus_Grafana_Jenkins/jenkins-server-terraform/07_iam-instance-profile.tf
@@ -1,4 +1,6 @@
-resource "aws_iam_instance_profile" "instance-profile" {
-  name = "jenkins-server-instance-profile"
-  role = aws_iam_role.iam-role.name
-}
+# resource "aws_iam_instance_profile" "instance-profile" {
+#   name = "jenkins-server-instance-profile"
+#   role = aws_iam_role.iam-role.name
+# }
+
+terraform import aws_iam_instance_profile.instance-profile jenkins-server-instance-profile
