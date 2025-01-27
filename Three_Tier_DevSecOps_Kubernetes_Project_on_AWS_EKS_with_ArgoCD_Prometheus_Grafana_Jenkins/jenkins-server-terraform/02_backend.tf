792dd7b3ff1e90084766e0db8b9039b0fd1945ec
men_l <men_long@yahoo.com>
2025-01-26 19:38:22 -0500

    update

--- Three_Tier_DevSecOps_Kubernetes_Project_on_AWS_EKS_with_ArgoCD_Prometheus_Grafana_Jenkins/jenkins-server-terraform/02_backend.tf
+++ Three_Tier_DevSecOps_Kubernetes_Project_on_AWS_EKS_with_ArgoCD_Prometheus_Grafana_Jenkins/jenkins-server-terraform/02_backend.tf
@@ -2,10 +2,10 @@
 terraform {
   backend "s3" {
     # The S3 bucket where the Terraform state file will be stored.
-    bucket         = "three-tier-devsecops-project-bucket-longmen"
+    bucket         = "three-tier-devsecops-project-bucket-longmen2"
     
     # The AWS region where the S3 bucket and DynamoDB table are located.
-    region         = "us-east-1"
+    region         = "us-west-2"
     
     # The key (path) within the S3 bucket for storing the state file.
     key            = "three-tier-devdecops-project/jenkins-server-terraform/terraform.tfstate"
