1e29d86a84dd6c4f18e7e5d84d7d236e305a7f13
men_l <men_long@yahoo.com>
2025-03-23 08:30:32 -0400

    update

--- Creating-RDS-Instance-Using-Terraform/modules/ec2/output.tf
+++
@@ -1,4 +0,0 @@
-output "sg_id" {
-    value = aws_security_group.ec2_sg.id
-  
-}
--- Creating-RDS-Instance-Using-Terraform/modules/secrets/output.tf
+++
@@ -1,4 +0,0 @@
-output "db_password" {
-    value = jsondecode(aws_secretsmanager_secret_version.secret_version.secret_string)
-  
-}
